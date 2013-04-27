2e43a00a827b4338432c5ea1bb86df9afeb886fb
Scott Boyle <scott@monospaced.co.uk>
2013-04-27 20:59:51 +0100

    v0.0.2

--- elastic.js
+++ elastic.js
@@ -1,36 +1,12 @@
 /*
- * angular-elastic v0.0.1
+ * angular-elastic v0.0.2
  * (c) 2013 Monospaced http://monospaced.com
  * License: MIT
  */
 
-(function(){
-  'use strict';
-
-  var elastic = angular.module('monospaced.elastic', []),
-                mimics = ['paddingTop',
-                          'paddingRight',
-                          'paddingBottom',
-                          'paddingLeft',
-                          'fontSize',
-                          'lineHeight',
-                          'fontFamily',
-                          'width',
-                          'fontWeight',
-                          'border-top-width',
-                          'border-right-width',
-                          'border-bottom-width',
-                          'border-left-width',
-                          'borderTopStyle',
-                          'borderTopColor',
-                          'borderRightStyle',
-                          'borderRightColor',
-                          'borderBottomStyle',
-                          'borderBottomColor',
-                          'borderLeftStyle',
-                          'borderLeftColor'];
-
-  elastic.directive('msdElastic', function(){
+angular.module('monospaced.elastic', [])
+  .directive('msdElastic', ['$window', '$timeout', function($window, $timeout){
+    'use strict';
     return {
       restrict: 'A, C',
       link: function(scope, element){
@@ -40,10 +16,17 @@
           return;
         }
 
+        // Hide scrollbars
+        element.css({
+          'overflow': 'hidden',
+          'resize': 'none'
+        });
+
         var domTwin = document.createElement('div'),
             twin = angular.element(domTwin).css({
               'position': 'absolute',
-              'visibility': 'hidden',
+              'top': '-10000px',
+              'left': '-10000px',
               'word-wrap': 'break-word',
               'white-space': 'pre-wrap'
             }),
@@ -51,7 +34,18 @@
             lineHeight  = parseInt(domElmStyle.getPropertyValue('line-height'), 10) || parseInt(domElmStyle.getPropertyValue('font-size'), 10),
             minHeight = parseInt(domElmStyle.getPropertyValue('height'), 10) || lineHeight * 3,
             maxHeight = parseInt(domElmStyle.getPropertyValue('max-height'), 10) || Number.MAX_VALUE,
-            goalHeight = 0;
+            goalHeight = 0,
+            mimics = ['padding-right',
+                      'padding-left',
+                      'font-size',
+                      'line-height',
+                      'font-family',
+                      'width',
+                      'font-weight',
+                      'border-right-width',
+                      'border-right-style',
+                      'border-left-width',
+                      'border-left-style'];
 
         // Opera returns max-height of -1 if not set
         if (maxHeight < 0) {
@@ -94,11 +88,30 @@
           }
         }
 
+        // Compact textarea on blur
+        function compact() {
+          var twinHeight = domTwin.offsetHeight;
+          if (twinHeight < maxHeight) {
+            if (twinHeight > minHeight) {
+              element.css({
+                'height': twinHeight + 'px'
+              });
+            } else {
+              element.css({
+                'height': minHeight + 'px'
+              });
+            }
+          }
+        }
+
         // This function will update the height of the textarea if necessary
         function update(forced) {
 
           // Get curated content from the textarea
-          var textareaContent = element.val().replace(/&/g,'&amp;').replace(/ {2}/g, '&nbsp;').replace(/<|>/g, '&gt;').replace(/\n/g, '<br />');
+          var textareaContent = element.val().replace(/&/g,'&amp;')
+                              .replace(/ {2}/g, '&nbsp;')
+                              .replace(/<|>/g, '&gt;')
+                              .replace(/\n/g, '<br />');
 
           // Compare curated content with curated twin
           var twinContent = twin.html().replace(/<br>/ig,'<br />');
@@ -112,7 +125,6 @@
             var twinHeight = domTwin.offsetHeight;
             if (Math.abs((twinHeight + lineHeight) - domElm.offsetHeight) > 3) {
               var goalHeight = twinHeight + lineHeight;
-
               if (goalHeight >= maxHeight) {
                 setHeightAndOverflow(maxHeight, 'auto');
               } else if (goalHeight <= minHeight) {
@@ -124,11 +136,6 @@
           }
         }
 
-        // Hide scrollbars
-        element.css({
-          'overflow': 'hidden'
-        });
-
         // Update textarea size on keyup, change, cut and paste
         element.bind('keyup change cut paste', function(){
           update();
@@ -136,35 +143,35 @@
 
         // Update width of twin if browser or textarea is resized
         // (solution for textareas with widths in percent)
-        angular.element(window).bind('resize', setTwinWidth);
+        angular.element($window).bind('resize', setTwinWidth);
         element.bind('resize', setTwinWidth);
         element.bind('update', update);
 
         // Compact textarea on blur
-        element.bind('blur', function(){
-          var twinHeight = domTwin.offsetHeight;
-          if (twinHeight < maxHeight) {
-            if (twinHeight > minHeight) {
-              element.css({
-                'height': twinHeight + 'px'
-              });
-            } else {
-              element.css({
-                'height': minHeight + 'px'
-              });
-            }
-          }
-        });
+        element.bind('blur', compact);
 
         // Catch the browser paste event
         element.bind('input paste', function(){
-          setTimeout(update, 250);
+          $timeout(update, 250);
         });
 
         // Run update once initialised
         update();
+        compact();
+
+        // apply animations after initalisation
+        $timeout(function(){
+          element.css({
+            '-webkit-transition': 'height 50ms ease-in-out',
+               '-moz-transition': 'height 50ms ease-in-out',
+                 '-o-transition': 'height 50ms ease-in-out',
+                    'transition': 'height 50ms ease-in-out'
+          });
+        });
+
+        scope.$on('$destroy', function() {
+           twin.remove();
+        });
       }
     };
-  });
-
-})();
+  }]);
