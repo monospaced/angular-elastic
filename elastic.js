6c62c3c5c8e5ec58345a5408d83aab85ef235390
Scott Boyle <scott@monospaced.co.uk>
2013-04-27 22:57:07 +0100

    v0.0.3

--- elastic.js
+++ elastic.js
@@ -1,5 +1,5 @@
 /*
- * angular-elastic v0.0.2
+ * angular-elastic v0.0.3
  * (c) 2013 Monospaced http://monospaced.com
  * License: MIT
  */
@@ -7,79 +7,92 @@
 angular.module('monospaced.elastic', [])
   .directive('msdElastic', ['$window', '$timeout', function($window, $timeout){
     'use strict';
+
     return {
       restrict: 'A, C',
       link: function(scope, element){
+
+        /*
+         * setup
+         */
+
+        // cache a reference to the DOM element
         var domElm = element[0];
 
+        // ensure element is a textarea
         if (domElm.nodeName !== 'TEXTAREA') {
           return;
         }
 
-        // Hide scrollbars
+        // set these properties before measuring dimensions
         element.css({
           'overflow': 'hidden',
           'resize': 'none'
         });
 
-        var domTwin = document.createElement('div'),
+        var windowElm = angular.element($window),
+            domTwin = document.createElement('div'),
             twin = angular.element(domTwin).css({
               'position': 'absolute',
               'top': '-10000px',
               'left': '-10000px',
-              'word-wrap': 'break-word',
-              'white-space': 'pre-wrap'
+              'white-space': 'pre-wrap',
+              'word-wrap': 'break-word'
             }),
             domElmStyle = getComputedStyle(domElm),
-            lineHeight  = parseInt(domElmStyle.getPropertyValue('line-height'), 10) || parseInt(domElmStyle.getPropertyValue('font-size'), 10),
+            lineHeight = parseInt(domElmStyle.getPropertyValue('line-height'), 10) ||
+                         parseInt(domElmStyle.getPropertyValue('font-size'), 10),
             minHeight = parseInt(domElmStyle.getPropertyValue('height'), 10) || lineHeight * 3,
             maxHeight = parseInt(domElmStyle.getPropertyValue('max-height'), 10) || Number.MAX_VALUE,
-            goalHeight = 0,
-            mimics = ['padding-right',
-                      'padding-left',
-                      'font-size',
-                      'line-height',
-                      'font-family',
-                      'width',
-                      'font-weight',
-                      'border-right-width',
-                      'border-right-style',
-                      'border-left-width',
-                      'border-left-style'];
+            targetHeight = 0,
+            properties = ['padding-right',
+                          'padding-left',
+                          'border-right-width',
+                          'border-right-style',
+                          'border-left-width',
+                          'border-left-style',
+                          'width',
+                          'font-weight',
+                          'font-size',
+                          'font-family',
+                          'line-height'];
 
         // Opera returns max-height of -1 if not set
         if (maxHeight < 0) {
           maxHeight = Number.MAX_VALUE;
         }
 
-        // Append the twin to the DOM
-        // We are going to meassure the height of this, not the textarea.
-        element.parent().append(twin);
-
-        // Copy the essential styles from the textarea to the twin
-        var i = mimics.length,
+        // copy the essential styles from the textarea to the twin
+        var i = properties.length,
             property;
         while(i--){
-          property = mimics[i].toString();
+          property = properties[i].toString();
           twin.css(property, domElmStyle.getPropertyValue(property));
         }
 
-        // Updates the width of the twin
-        // (solution for textareas with widths in percent)
+        // append the twin to the DOM
+        element.parent().append(twin);
+
+        /*
+         * methods
+         */
+
         function setTwinWidth(){
+          // textareas with width set in percent can change size on resize events
           var curatedWidth = Math.floor(parseInt(domElm.offsetWidth, 10));
+
           if (domTwin.offsetWidth !== curatedWidth) {
             twin.css({
               'width': curatedWidth + 'px'
             });
-            // Update height of textarea
-            update(true);
+            stretch(true);
           }
         }
 
-        // Sets a given height and overflow state on the textarea
-        function setHeightAndOverflow(height, overflow){
+        function setElmHeight(height, overflow){
+          // apply the required textarea height
           var curratedHeight = Math.floor(parseInt(height, 10));
+
           if (domElm.offsetHeight !== curratedHeight) {
             element.css({
               'height': curratedHeight + 'px',
@@ -88,9 +101,10 @@ angular.module('monospaced.elastic', [])
           }
         }
 
-        // Compact textarea on blur
-        function compact() {
+        function shrink(){
+          // remove the extra line required for smooth stretching
           var twinHeight = domTwin.offsetHeight;
+
           if (twinHeight < maxHeight) {
             if (twinHeight > minHeight) {
               element.css({
@@ -104,62 +118,58 @@ angular.module('monospaced.elastic', [])
           }
         }
 
-        // This function will update the height of the textarea if necessary
-        function update(forced) {
+        function stretch(forced){
+          // stretch the textarea if required
+          var twinHeight,
+              twinContent = twin.html().replace(/<br>/ig, '<br />'),
+              elementContent = element.val().replace(/&/g, '&amp;')
+                                            .replace(/ {2}/g, '&nbsp;')
+                                            .replace(/<|>/g, '&gt;')
+                                            .replace(/\n/g, '<br />');
 
-          // Get curated content from the textarea
-          var textareaContent = element.val().replace(/&/g,'&amp;')
-                              .replace(/ {2}/g, '&nbsp;')
-                              .replace(/<|>/g, '&gt;')
-                              .replace(/\n/g, '<br />');
+          if (forced || elementContent + '&nbsp;' !== twinContent) {
+            // add extra white space so new rows are added when at the end of a row
+            twin.html(elementContent + '&nbsp;');
 
-          // Compare curated content with curated twin
-          var twinContent = twin.html().replace(/<br>/ig,'<br />');
+            // measure the twin
+            twinHeight = domTwin.offsetHeight;
 
-          if (forced || textareaContent + '&nbsp;' !== twinContent) {
-            // Add an extra white space so new rows are added when you are at the end of a row
-            twin.html(textareaContent + '&nbsp;');
-
-            // Change textarea height
-            // if twin + the height of 1 line differs more than 3 pixels from textarea height
-            var twinHeight = domTwin.offsetHeight;
+            // compare twin height and textarea height
             if (Math.abs((twinHeight + lineHeight) - domElm.offsetHeight) > 3) {
-              var goalHeight = twinHeight + lineHeight;
-              if (goalHeight >= maxHeight) {
-                setHeightAndOverflow(maxHeight, 'auto');
-              } else if (goalHeight <= minHeight) {
-                setHeightAndOverflow(minHeight, 'hidden');
-              } else {
-                setHeightAndOverflow(goalHeight, 'hidden');
+              // textarea needs to stretch, find the required height
+              targetHeight = twinHeight + lineHeight;
+              if (targetHeight >= maxHeight) {
+                return setElmHeight(maxHeight, 'auto');
               }
+              if (targetHeight <= minHeight) {
+                return setElmHeight(minHeight, 'hidden');
+              }
+              return setElmHeight(targetHeight, 'hidden');
             }
           }
         }
 
-        // Update textarea size on keyup, change, cut and paste
-        element.bind('keyup change cut paste', function(){
-          update();
-        });
+        /*
+         * initialise
+         */
 
-        // Update width of twin if browser or textarea is resized
-        // (solution for textareas with widths in percent)
-        angular.element($window).bind('resize', setTwinWidth);
-        element.bind('resize', setTwinWidth);
-        element.bind('update', update);
+        // listen
+        element
+          .bind('keyup change cut paste', stretch)
+          .bind('input paste', function(){
+            // catch the browser paste event
+            $timeout(stretch, 250);
+          })
+          .bind('blur', shrink)
+          .bind('resize', setTwinWidth);
 
-        // Compact textarea on blur
-        element.bind('blur', compact);
+        windowElm.bind('resize', setTwinWidth);
 
-        // Catch the browser paste event
-        element.bind('input paste', function(){
-          $timeout(update, 250);
-        });
+        // set dimensions
+        stretch();
+        shrink();
 
-        // Run update once initialised
-        update();
-        compact();
-
-        // apply animations after initalisation
+        // apply animations only after setting dimenmsions
         $timeout(function(){
           element.css({
             '-webkit-transition': 'height 50ms ease-in-out',
@@ -169,9 +179,15 @@ angular.module('monospaced.elastic', [])
           });
         });
 
-        scope.$on('$destroy', function() {
-           twin.remove();
+        /*
+         * destroy
+         */
+
+        scope.$on('$destroy', function(){
+          twin.remove();
+          windowElm.unbind('resize', setTwinWidth);
         });
       }
     };
+
   }]);
