ece0ee20782f809b613d505b3e9e9e12c2c7ae98
c4605 <bolasblack@gmail.com>
2015-03-27 21:12:25 +0800

    support other style width with jquery

--- elastic.js
+++ elastic.js
@@ -121,7 +121,9 @@ angular.module('monospaced.elastic', [])
                 taComputedStyleWidth,
                 mirrorHeight,
                 width,
-                overflow;
+                overflow,
+                ngHided,
+                jqHided;
 
             if (mirrored !== ta) {
               initMirror();
@@ -143,6 +145,14 @@ angular.module('monospaced.elastic', [])
                 // update mirror width in case the textarea width has changed
                 width = parseInt(taComputedStyleWidth, 10) - boxOuter.width;
                 mirror.style.width = width + 'px';
+              } else if (element.jquery) {
+                ngHided = element.hasClass('ng-hide');
+                jqHided = !ngHided && element.css('display') === 'none';
+                if (ngHided) { element.removeClass('ng-hide'); }
+                if (jqHided) { element.show(); }
+                mirror.style.width = element.width() + 'px';
+                if (ngHided) { element.addClass('ng-hide') }
+                if (jqHided) { element.hide(); }
               }
 
               mirrorHeight = mirror.scrollHeight;
